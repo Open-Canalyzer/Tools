8e65cb42c3f023fc3786f38665d0d89a55bb64a4
maartentamboer <maartentamboer@msn.com>
2016-08-12 17:36:46 +0200

    Added more logic and states to slcan parser

--- slcan/slcan.c
+++ slcan/slcan.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+bool AutoPoll = false;
+bool CanOpened = false;
 
 
 void PrintCommand(const char* Command, int size)
@@ -46,8 +50,62 @@ char CanSetup(char Mode)
     ret = SLCAN_ERR;
 
   //Check if the can bus is actually closed;
+  if(CanOpened)
+    ret = SLCAN_ERR;
 
   //Setup the can mode
+  if(ret != SLCAN_ERR)
+  {
+    //Setup the CAN mode
+    //It probably needs to be saved until OpenCanBus is called
+  }
+
+  return ret;
+}
+
+char OpenCanBus(char mode)
+{
+  char ret = SLCAN_OK;
+
+  switch (mode)
+  {
+    case SLCAN_CMD_OPEN:    //Implement
+    case SLCAN_CMD_LISTEN:
+        CanOpened = true;
+      break;
+    default:
+      ret = SLCAN_ERR;
+  }
+
+  return ret;
+}
+
+char CloseCanBus(void)
+{
+  char ret = SLCAN_OK;
+
+  CanOpened = false;
+
+  return ret;
+}
+
+//AutoPollMode is the preferred way of working
+char AutoPollMode(char mode)
+{
+  char ret = SLCAN_OK;
+
+  if(CanOpened)
+    ret = SLCAN_ERR;
+
+  if(ret != SLCAN_ERR)
+  {
+    if(mode == '0')
+      AutoPoll = false;
+    else if(mode == '1')
+      AutoPoll = true;
+    else
+      ret = SLCAN_ERR;
+  }
 
   return ret;
 }
@@ -62,13 +120,54 @@ char ParseCommand(const char* in, int commandsize)
     case SLCAN_CMD_SETUP:
       ret = CanSetup(in[1]);
       break;
+    case SLCAN_CMD_SETUP_BTR:
+      ret = SLCAN_ERR_NOT_IMPLEMENTED;
+      break;
     case SLCAN_CMD_OPEN:
+    case SLCAN_CMD_LISTEN:
+      ret = OpenCanBus(in[0]);
       break;
     case SLCAN_CMD_CLOSE:
+      ret = CloseCanBus();
       break;
     case SLCAN_CMD_TX11:
       break;
+    case SLCAN_CMD_TX29:
+      break;
+    case SLCAN_CMD_RTR11:
+      break;
+    case SLCAN_CMD_RTR29:
+      break;
+    case SLCAN_CMD_POLL_ONE:
+      break;
+    case SLCAN_CMD_POLL_MANY:
+      break;
+    case SLCAN_CMD_FLAGS:
+      break;
+    case SLCAN_CMD_FILTER:
+      ret = SLCAN_ERR_NOT_IMPLEMENTED;
+      break;
+    case SLCAN_CMD_ACC_CODE:
+      ret = SLCAN_ERR_NOT_IMPLEMENTED;
+      break;
+    case SLCAN_CMD_ACC_MASK:
+      ret = SLCAN_ERR_NOT_IMPLEMENTED;
+      break;
+    case SLCAN_CMD_UART:
+      ret = SLCAN_ERR_NOT_IMPLEMENTED;
+      break;
+    case SLCAN_CMD_VERSION1:
+    case SLCAN_CMD_VERSION2:
+      break;
+    case SLCAN_CMD_SERIAL:
+      break;
+    case SLCAN_CMD_TIMESTAMP:
+      break;
+    case SLCAN_CMD_AUTOSTART:
+      ret = SLCAN_ERR_NOT_IMPLEMENTED;
+      break;
     case SLCAN_CMD_AUTOPOLL:
+      ret = AutoPollMode(in[1]);
       break;
     default:
       ret = SLCAN_ERR_UNKNOWN_CMD;
